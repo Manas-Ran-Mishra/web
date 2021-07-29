07e5870c4598ea31d4160ae8e4a7e9acf7309ac4
Manas Ranjan Mishra <manasran.mishra2@gmail.com>
2021-07-30 00:32:19 +0530

    understanding a function

--- JS_Fundaments(CodeAddict)/app.js
+++ JS_Fundaments(CodeAddict)/app.js
@@ -2,9 +2,24 @@
 
 function greet(username) {
   console.log("Hello " + username);
+  return "calling this function shouldn't show undefined at the end";
 }
 
-// console.log(username);
+// Function without return type
+function greet2(yourname) {
+  console.log("Hi " + yourname + " from greet2()");
+}
 
 // Invoking function
-greet("BOB");
+greet("Bob");
+
+// Directly invoking the funtion inside log()
+console.log(greet("bOB"));
+
+// Printing the result of the return type
+let result;
+result = greet("BOB");
+console.log(result);
+
+console.log("Try invoking the funtion greet2() from the console lke below");
+console.log(">greet2('Alice')");
