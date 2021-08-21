e6c6ad6d16c4ffd85c599f46d0eb9c190bffa355
Manas Ranjan Mishra <manasran.mishra2@gmail.com>
2021-08-22 01:21:35 +0530

    updates

--- jspractice.js
+++ jspractice.js
@@ -1,14 +1,39 @@
-function newjsfunc(){
-	alert("Hello World");
+function newjsfunc() {
+  // alert('Hello World');
+  document.getElementById('submitted').innerHTML = 'Form Submitted';
 }
-function validatejsfunc(){
-	
-	//var rt = /a+b/;
-	var password = document.getElementById('validate').value;
-	if (password != "abcd123") {alert("wrong format");}
+
+let password;
+
+function validatejsfunc() {
+  //var rt = /a+b/;
+  password = document.getElementById('validate').value;
+  if (emptypassword(password) !== true) {
+    alert('Wrong. Enter password in correct format');
+  }
 }
-function confirmedpasswordjsfunc(){
-	var cpassword = document.getElementById('confirm').value;
-	if (cpassword!= "abcd123") {alert("wrong format");}
-	if (cpassword!=password) {alert("password does not match");}
+function confirmedpasswordjsfunc() {
+  let cpassword = document.getElementById('confirm').value;
+  if (emptypassword(password) && emptypassword(cpassword)) {
+    if (cpassword !== password) {
+      // alert('password does not match');
+      document.getElementById('float2').innerHTML = 'password does not match';
+    } else if (cpassword === password) {
+      document.getElementById('float2').innerHTML = '✅';
+    }
+  }
+}
+
+function emptypassword(currentPassword) {
+  if (
+    currentPassword !== undefined ||
+    currentPassword !== null ||
+    currentPassword !== ''
+  ) {
+    return true;
+  } else {
+    document.getElementById('float1').innerHTML = 'Should not be empty';
+    return false;
+    // console.log(`Please enter a password`);
+  }
 }
